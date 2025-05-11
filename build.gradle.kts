b18caf5c4d52e6f99bcd94df6e39153c378de1f2
DC <105041921+ghostyapps@users.noreply.github.com>
2025-05-12 01:08:32 +0300

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,53 @@
+import com.android.build.gradle.tasks.PackageAndroidArtifact
 plugins {
-    id("com.android.application") version "8.9.2" apply false
-    kotlin("android") version "1.9.20" apply false
+    id("com.android.application")
+    kotlin("android")
 }
+
+android {
+    namespace = "com.dcapps.heyreminder"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.dcapps.heyreminder"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.4"
+    }
+
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.material3:material3:1.1.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.compose.material:material-icons-core:1.5.0")
+    implementation("androidx.compose.material:material-icons-extended:1.5.0")
+    // Material3 Date & Time Pickers
+    implementation("com.marosseleng.android:compose-material3-datetime-pickers:0.7.2")
+    implementation ("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+
+}
+
+
